6c040815f44f1d23264d91b1bb6c2414da805021
Junek3 <rlarudrms28@gmail.com>
2020-12-21 18:43:57 +0900

    Start blog

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Console.dir(junek3)`,
+  description: `Blog posting about my developer life`,
+  author: `junek3`,
+  introduction: `백엔드 개발자를 목표로 공부중인 대학생입니다. 웹개발과 데이터 엔지니어링에 관심이 많습니다. 주로 Node.js와 React로 개발하고 있고 Typescript 도입을 고민하고 있습니다.`,
+  siteUrl: `http://localhost:8000`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `junek3`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -21,10 +21,10 @@ module.exports = {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  // ga: '', // Add your google analytics tranking ID
 }
